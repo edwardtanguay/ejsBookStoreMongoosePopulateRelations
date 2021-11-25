8af4d984d2d59897d97bdb533f7527a42e5a702c
Edward Tanguay (hm.win) <edward@tanguay.info>
2021-11-25 02:20:38 +0100

    moved views

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const port = 3044;
 const staticDirectory = path.join(__dirname, './public');
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, './public/views'));
+app.set('views', path.join(__dirname, './src/views'));
 app.use(express.static(staticDirectory));
 
 app.get('/', async (req, res) => {
